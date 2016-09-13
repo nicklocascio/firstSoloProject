641099ad1208d77707f7c35958d0113a452b88ba
BrandonA614 <brandon.ashkin@darbygroup.com>
2016-09-13 14:49:06 -0600

    Finished Display, Finished X or O input

--- firstSoloProject/src/firstSoloProject/display.java
+++ firstSoloProject/src/firstSoloProject/display.java
@@ -1,36 +1,50 @@
 package firstSoloProject;
 import java.util.Scanner;
+import java.util.Arrays;
+
+@SuppressWarnings("resource")
 
 
 public class display
 	{
-
+		//User Variables (X or Os)
+		static String userChoice;
+		static String computerGiven;
+		
+		//Board Setup
+		static String[][] boardO = new String[3][3];
+		
+		
 		public static void main(String[] args)
 			{
+				fillArray();
 				Choice();
 				visualDisplay();
-				
-				
+
+				boardO[0][1] = userChoice;
 			}
 
-		
+		//Setup Board, Contains array within, basically complete
 		public static void visualDisplay(){
 			
-			System.out.println("________________");
-			System.out.println("|    "+"|    "+"|    "+"|");
-			System.out.println("|    "+"|    "+"|    "+"|");
-			System.out.println("|    "+"|    "+"|    "+"|");
-			System.out.println("________________");
-			System.out.println("|    "+"|    "+"|    "+"|");
-			System.out.println("|    "+"|    "+"|    "+"|");
-			System.out.println("|    "+"|    "+"|    "+"|");
-			System.out.println("________________");
-			System.out.println("|    "+"|    "+"|    "+"|");
-			System.out.println("|    "+"|    "+"|    "+"|");
-			System.out.println("|    "+"|    "+"|    "+"|");
-			System.out.println("________________");
+			System.out.println("     1    2    3");
+			System.out.println("   ___________________");
+			System.out.println("   |    "+" |    "+" |    "+" |");
+			System.out.println("A  " + "|  " + boardO[0][0] + "  |  " + boardO[1][0] + "  |  " + boardO[2][0] + "  |");
+			System.out.println("   |    "+" |    "+" |    "+" |");
+			System.out.println("   ___________________");
+			System.out.println("   |    "+" |    "+" |    "+" |");
+			System.out.println("B  "+"|  " + boardO[0][1] +"  |  " + boardO[1][1] +"  |  "+ boardO[2][1] +"  |");
+			System.out.println("   |    "+" |    "+" |    "+" |");
+			System.out.println("   ___________________");
+			System.out.println("   |    "+" |    "+" |    "+" |");
+			System.out.println("C  "+"|  " + boardO[0][2] +"  |  " + boardO[1][2] +"  |  " + boardO[2][2] +"  |");
+			System.out.println("   |    "+" |    "+" |    "+" |");
+			System.out.println("   ___________________");
 			
 		}
+		
+		//Completed Choice, Is able to choose either "X or O"
 		
 		public static void Choice(){
 			System.out.println("Would you like to be X's or O's? " );
@@ -53,5 +67,16 @@ public class display
 			
 		}
 		
-	}
+		public static void fillArray(){
+			 for (int i = 0; i < boardO.length; i++){
+			        Arrays.fill(boardO[i], "1");
+			    }
+						
+				
+			}
+			
+			
+		}
+		
+
 	
---
+++ firstSoloProject/src/firstSoloProject/userInput.java
@@ -0,0 +1,25 @@
+package firstSoloProject;
+import java.util.Scanner;
+
+
+public class userInput
+	{
+
+			
+		public static void locationChoice(){
+			boolean Win = false;
+			
+			
+			while(Win = false){
+				
+				System.out.println("Where would you like to go?");
+				Scanner locationInput = new Scanner(System.in);
+				String location = locationInput.nextLine();
+				
+				
+			}
+			
+			
+		}
+		
+	}
